4a60c2df31425ab71149aedde8554ff636abdfab
aoobao <aoobao@foxmail.com>
2024-03-23 10:39:47 +0800

    change base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/docs/",
+  base: "/direction-icon/",
   build: {
     outDir: "docs",
   },
